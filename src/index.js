b3340d36ad059ef185bb8a50a8cf81930b03b268
vladislav-slobodenjuk <vladjun@gmail.com>
2023-07-23 17:26:18 +0300

    another fix for basename address

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-fs78-react-hw-05-movies/">
-      {/* <BrowserRouter> */}
+    <BrowserRouter basename="/goit-fs78-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
